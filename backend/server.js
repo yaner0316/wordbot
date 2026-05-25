ba6b26718d2c259955d631b42cfad6d17ace549d
edwpan81-lab <edwpan81@gmail.com>
2026-05-25 22:45:38 +0800

    fix: 导出searchRecords函数

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { generateQuiz, submitAnswers, getStats, addWord, getAllUsers, getAllStats, validateWords, addWords, updateMultiDefinition, getWord, updateWord, deleteWord } = require('./feishu');
+const { generateQuiz, submitAnswers, getStats, addWord, getAllUsers, getAllStats, validateWords, addWords, updateMultiDefinition, getWord, updateWord, deleteWord, searchRecords } = require('./feishu');
 
 const app = express();
 app.use(cors());
